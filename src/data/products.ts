bb0805f7b938c710c8eebe404ef17b9f2887e720
Tejas Makwana <72186683+TejasMakwana29@users.noreply.github.com>
2026-08-25 06:29:56 +0000

    test change 2

--- src/data/products.ts
+++ src/data/products.ts
@@ -25,7 +25,7 @@ export const productCategories: ProductCategory[] = [
   // 1. RO Antiscalant
   {
     slug: "ro-dosing-chemicals",
-    name: "RO Antiscalant Chemical",
+    name: "RO Antiscalant Chemicals",
     description: "High-performance antiscalants for optimal membrane protection and system efficiency.",
     image: "/images/RO-dosing.jpg",
   },
